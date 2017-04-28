a954d4c5fcc597877620839597bd2c22f33b1f80
SwagStormer <ryan@ryansbees.com>
2017-04-27 22:08:10 -0600

    moved querysets to filterbackends

---
+++ course/filters.py
@@ -0,0 +1,89 @@
+from django.db.models import Q
+from rest_framework.filters import BaseFilterBackend
+from users.models import TeacherProfile, StudentProfile
+from course.models import Course, Hour, Assignment, AssignmentSubmission
+
+
+class CourseFilterBackend(BaseFilterBackend):
+
+    def filter_queryset(self, request, queryset, view):
+        q = request.query_params.get
+        user = request.user
+        if user.is_authenticated:
+            is_teacher = True if user.is_teacher else False
+            if is_teacher:
+                teacher = TeacherProfile.objects.get(user=user)
+                Course.objects.filter(teachers=teacher)
+            else:
+                student = StudentProfile.objects.filter(user=user)
+                queryset = Course.objects.filter(students__in=student)
+            return queryset
+        else:
+            pass
+
+
+class HourFilterBackend(BaseFilterBackend):
+
+    def filter_queryset(self, request, queryset, view):
+        q = request.query_params.get
+        user = request.user
+        if user.is_authenticated:
+            is_teacher = True if user.is_teacher else False
+            if is_teacher:
+                teacher = TeacherProfile.objects.filter(user=user)
+                courses = Course.objects.filter(teachers__in=teacher)
+            else:
+                student = StudentProfile.objects.filter(user=user)
+                courses = Course.objects.filter(students__in=student)
+            queryset = Hour.objects.filter(course=courses)
+
+            if q('course'):
+                queryset.filter(course=q('course'))
+            if q('search'):
+                queryset.filter(name__icontains=q('search'))
+            return queryset
+        else:
+            pass
+
+
+class AssignmentFilterBackend(BaseFilterBackend):
+
+    def filter_queryset(self, request, queryset, view):
+        queryset = Assignment.objects.all()
+        q = request.query_params.get
+        user = request.user
+        if user.is_authenticated:
+            is_teacher = True if user.is_teacher else False
+            if is_teacher:
+                teacher = TeacherProfile.objects.get(user=user)
+                courses = Course.objects.filter(teachers=teacher)
+            else:
+                student = StudentProfile.objects.get(user=user)
+                courses = Course.objects.filter(students=student)
+            queryset.filter(course=courses)
+            if q('course'):
+                queryset = queryset.filter(course=q('course'))
+            if q('search'):
+                queryset = queryset.filter(Q(name__icontains=q('search')) | Q(course__name__icontains=q('search')))
+        return queryset
+
+
+class AssignmentSubmissionFilterBackend(BaseFilterBackend):
+
+    def filter_queryset(self, request, queryset, view):
+        q = request.query_params.get
+        queryset = AssignmentSubmission.objects.all()
+        if q('graded'):
+            graded = True if q('graded') == 'true' else False
+            print(graded)
+            queryset = queryset.filter(graded=graded)
+        if q('course'):
+            queryset = queryset.filter(assignment__course=q('course'))
+        if q('student'):
+            queryset = queryset.filter(student=q('student'))
+        if q('search'):
+            queryset = queryset.filter(
+                Q(assignment__name__icontains=q('search')) | Q(student__user__first_name__icontains=q('search')))
+        if q('assignment'):
+            queryset = queryset.filter(assignment=q('assignment'))
+        return queryset
--- course/views.py
+++ course/views.py
@@ -5,30 +5,16 @@ from course.serializers import CourseSerializer, PeriodSerializer, AssignmentSer
 from course.models import Course, Period, Assignment, AssignmentSubmission, Hour
 from users.models import TeacherProfile, StudentProfile
 from django.db.models import Q
-
-
+from course.filters import CourseFilterBackend, HourFilterBackend, AssignmentFilterBackend, AssignmentSubmissionFilterBackend
 # Create your views here.
 
 
 class CourseViewSet(viewsets.ModelViewSet):
     serializer_class = CourseSerializer
     queryset = Course.objects.all()
-
-    def get_queryset(self):
-        queryset = Course.objects.all()
-        q = self.request.query_params.get
-        user = self.request.user
-        if user.is_authenticated:
-            is_teacher = True if user.is_teacher else False
-            if is_teacher:
-                teacher = TeacherProfile.objects.get(user=user)
-                Course.objects.filter(teachers=teacher)
-            else:
-                student = StudentProfile.objects.filter(user=user)
-                queryset = Course.objects.filter(students__in=student)
-            return queryset
-        else:
-            pass
+    filter_backends = [
+        CourseFilterBackend
+    ]
 
 
 class PeriodViewSet(viewsets.ModelViewSet):
@@ -39,32 +25,18 @@ class PeriodViewSet(viewsets.ModelViewSet):
 class HourViewSet(viewsets.ModelViewSet):
     serializer_class = HourSerializer
     queryset = Hour.objects.none()
-
-    def get_queryset(self):
-        q = self.request.query_params.get
-        user = self.request.user
-        if user.is_authenticated:
-            is_teacher = True if user.is_teacher else False
-            if is_teacher:
-                teacher = TeacherProfile.objects.filter(user=user)
-                courses = Course.objects.filter(teachers__in=teacher)
-            else:
-                student = StudentProfile.objects.filter(user=user)
-                courses = Course.objects.filter(students__in=student)
-            queryset = Hour.objects.filter(course=courses)
-
-            if q('course'):
-                queryset.filter(course=q('course'))
-            if q('search'):
-                queryset.filter(name__icontains=q('search'))
-            return queryset
-        else:
-            pass
+    filter_backends = [
+        HourFilterBackend
+    ]
 
 
 class AssignmentViewSet(viewsets.ModelViewSet):
     serializer_class = AssignmentSerializer
     queryset = Assignment.objects.all()
+    filter_backends = [
+        AssignmentFilterBackend
+    ]
+
 
     def get_serializer_class(self):
         if self.request.method == "POST":
@@ -73,43 +45,9 @@ class AssignmentViewSet(viewsets.ModelViewSet):
             return AssignmentSerializer
 
 
-    def get_queryset(self):
-        queryset = Assignment.objects.all()
-        q = self.request.query_params.get
-        user = self.request.user
-        if user.is_authenticated:
-            is_teacher = True if user.is_teacher else False
-            if is_teacher:
-                teacher = TeacherProfile.objects.get(user=user)
-                courses = Course.objects.filter(teachers=teacher)
-            else:
-                student = StudentProfile.objects.get(user=user)
-                courses = Course.objects.filter(students=student)
-            queryset.filter(course=courses)
-            if q('course'):
-                queryset = queryset.filter(course=q('course'))
-            if q('search'):
-                queryset = queryset.filter(Q(name__icontains=q('search')) | Q(course__name__icontains=q('search')))
-        return queryset
-
-
 class AssignmentSubmissionViewSet(viewsets.ModelViewSet):
     serializer_class = AssignmentSubmissionSerializer
     queryset = AssignmentSubmission.objects.all()
-
-    def get_queryset(self):
-        q = self.request.query_params.get
-        queryset = AssignmentSubmission.objects.all()
-        if q('graded'):
-            graded = True if q('graded') == 'true' else False
-            print(graded)
-            queryset = queryset.filter(graded=graded)
-        if q('course'):
-            queryset = queryset.filter(assignment__course=q('course'))
-        if q('student'):
-            queryset = queryset.filter(student=q('student'))
-        if q('search'):
-            queryset = queryset.filter(Q(assignment__name__icontains=q('search')) | Q(student__user__first_name__icontains=q('search')))
-        if q('assignment'):
-            queryset = queryset.filter(assignment=q('assignment'))
-        return queryset
+    filter_backends = [
+        AssignmentSubmissionFilterBackend
+    ]
